2ee65396c1543dc8c8a64eb497a96fe717df539a
Anthony Stivers <tony.stivers@gmail.com>
2018-06-18 14:01:47 -0400

    Add repro

---
+++ ModelStateIsValidRepro/Pages/Repro.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelStateIsValidRepro.Pages
+{
+    [BindProperties]
+    public class ReproModel : PageModel
+    {
+        [Required]
+        public string TestProperty {get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            return Page();
+        }
+    }
+}
--- ModelStateIsValidRepro/Startup.cs
+++ ModelStateIsValidRepro/Startup.cs
@@ -32,7 +32,8 @@ namespace ModelStateIsValidRepro
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
+                //.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
